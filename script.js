aeb9d1ff70148ea3733381cde969929e30f38efd
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-14 20:05:04 +0200

    dz 3.2

--- script.js
+++ script.js
@@ -1,50 +1,8 @@
 
 
-const x = 10;
-console.log(x);
-const type = typeof (x);
-console.log(type);
 
-
-let y = 20;
-console.log(y);
-const typey = typeof (y);
-console.log(typey);
-
-
-
-let z = 500;
-console.log(z);
-const typez = typeof (z);
-console.log(typez)
-
-
-const max_orang = 12347937568345n;
-console.log(max_orang)
-const typen = typeof (max_orang);
-console.log(typen)
-
-
-
-let welcom = "Hello Viktor"
-console.log(welcom)
-const typestring = typeof (welcom);
-console.log(typestring);
-
-
-let itisnotsunnytoday= false;
-console.log(itisnotsunnytoday);
-const typef = typeof (itisnotsunnytoday);
-console.log(typef);
-
-
-let banan = null ;
-console.log(banan)
-const typeban = typeof (banan);
-console.log(typeban);
-
-
-let  citrus = undefined;
-console.log(citrus)
-const typecitrus = typeof (citrus);
-console.log(typecitrus);
+const first_name = prompt("What is your name?");
+const first_famely = prompt("what is your famely?");
+const ages = prompt("how old are you");
+const userInfo = `${first_name} ${first_famely} is ${ages} years old`;
+console.log(userInfo);
